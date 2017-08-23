a05d67b3a95b7ff661eb2e9dd3af2ac2b8e12423
Aitlamsali <italaa86@gmail.com>
2017-08-23 17:23:17 +0100

    Stud

--- student.py
+++ student.py
@@ -13,3 +13,44 @@ STATE = [('draft','Draft'),
         ('',''),
          ]
 
+class student_student(models.Model):
+    _name = 'student.student'
+    _description =  'Student Information'
+
+    name=fields.Char(string='Name', required=True, index=True)
+    active =fields.Boolean(string='Active', default=true)
+    image=fields.Binary(string='Image')
+    uni_no=fields.Char(string='University Number', required=True, copy=false)
+    seat_no=fields.Integer(string='Name', required=True, index=True, copy=false, default=  , store= , compute= , readonly= )
+    dob=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    age=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    gender=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+
+    result_ids=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    hobbies_ids=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    responsible_id=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    email=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    phone=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    fdate=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    ldate=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    degree_id=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    regfees=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    tutfees=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    totfees=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    ref=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    ref_link=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    health_issues=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    health_notes=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+    template=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+    state=fields.Char(string='Name', required=True, index=True, copy=True, default=  , store= , compute= , readonly= )
+
+
+
