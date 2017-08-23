08201b4555f50db3e0531c7f2e69fb8d41855cbd
Aitlamsali <italaa86@gmail.com>
2017-08-23 23:12:25 +0100

    sss

--- student.py
+++ student.py
@@ -9,8 +9,8 @@ STATE = [('draft','Draft'),
         ('second_register', 'Second Year Registred'),
         ('third_register', 'Third Year Registred'),
         ('fourth_register', 'Fourth Year Registred'),
+        ('dismiss', 'Dismissed'),
         ('allumi','Allumi'),
-        ('',''),
          ]
 
 class student_student(models.Model):
@@ -18,10 +18,10 @@ class student_student(models.Model):
     _description =  'Student Information'
 
     name=fields.Char(string='Name', required=True, index=True)
-    active =fields.Boolean(string='Active', default=true)
+    active =fields.Boolean(string='Active', default=True)
     image=fields.Binary(string='Image')
-    uni_no=fields.Char(string='University Number', required=True, copy=false)
-    seat_no=fields.Char(string='Seat No', copy=false)
+    uni_no=fields.Char(string='University Number', required=True, copy=False)
+    seat_no=fields.Char(string='Seat No', copy=False)
     dob=fields.Date(string='Date of Birth', required=True)
     age=fields.Integer(string='Age')
     gender=fields.Selection([('male','Male'),('female','Female')], 'Gender', default='male')
@@ -31,25 +31,26 @@ class student_student(models.Model):
 
     responsible_id=fields.Many2One('res.partner', 'Responsible Person / NOK')
     email=fields.Char(related='responsible_id.email')
-    phone=fields.Char(related= 'responsible_id.phone')
+    phone=fields.Char(related='responsible_id.phone')
 
     fdate=fields.Date(string='First Registration Date')
-    ldate=fields.Char(string='First Registration Date')
+    ldate=fields.Date(string='Last Registration Date')
 
     degree_id=fields.Many2One('degree.detail', 'Degree to Register For')
 
     regfees=fields.Float(string='Registration Fees', default='0.0')
     tutfees=fields.Float(string='Tuition Fees', default='0.0')
-    totfees=fields.Float(string='Total fees', store=true, readonly=true, compute='')
+    totfees=fields.Float(string='Total fees', store=True, readonly=True, compute='_get_total_fees')
 
     ref=fields.reference(selection=[('res.partner','Partner'),('res.user','User'),('student.student', 'Student')], string='Reference')
+
     ref_link=fields.Char(string='External Link')
 
     health_issues=fields.Selection([('yes','Yes'),('no','No')],string='Health Issues', default='no')
-    health_notes=fields.Text(string='Health Notes', copy=false )
+    health_notes=fields.Text(string='Health Notes', copy=False )
     template=fields.Html(string='Template')
 
-    state=fields.Selection(STATE, 'Status', readony=true, default='draft')
+    state=fields.Selection(STATE, 'Status', readony=True, default='draft')
 
 
 
